d2b85c5fdb6fcae3df71b51cd64fac223df547d4
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-09-17 01:34:38 +0530

    Add project files.

---
+++ TextToSpeech/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+
+namespace WPFTextToSpeech
+{
+    /// <summary> 
+    /// Interaction logic for MainWindow.xaml 
+    /// </summary> 
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(textBoxz.Text);
+            if (textBoxz.Text != "")
+            {
+                SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                //MessageBox.Show(textBoxz.Text);
+                speechSynthesizer.Speak(textBoxz.Text);
+            }
+            else
+            {
+                MessageBox.Show("Write some thing in the textbox!");
+            }
+        }
+    }
+}
